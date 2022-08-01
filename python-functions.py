7d5e149fa011573948d21113da73860dd9b75a70
nathanth <nathan.hardenburg@icloud.com>
2022-08-01 16:10:58 -0500

    completes challenge 3

--- python-functions.py
+++ python-functions.py
@@ -13,12 +13,24 @@
 
 # Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
-def largest(arr):
-  largest = 0
-  for n in arr:
-    if n > largest:
-      largest = n
-  return print(largest)
+# def largest(arr):
+#   largest = 0
+#   for n in arr:
+#     if n > largest:
+#       largest = n
+#   return print(largest)
 
-largest([1, 2, 3, 4, 0])  # returns 4
-largest([10, 4, 2, 231, 91, 54])  # returns 231
+# largest([1, 2, 3, 4, 0])  # returns 4
+# largest([10, 4, 2, 231, 91, 54])  # returns 231
+
+# ---------------------------------------------------------------------------
+
+# Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
+
+def occurrences(str1, str2):
+  return print(str1.count(str2))
+
+occurrences('fleep floop', 'e')   # returns 2
+occurrences('fleep floop', 'p')   # returns 2
+occurrences('fleep floop', 'ee')  # returns 1
+occurrences('fleep floop', 'fe')  # returns 0
